fef986619851de901395e8f83ba1ef2752a343db
Tom <tomrhernandez@gmail.com>
2017-08-23 03:18:50 +0000

    Application layout enhancements. Add partials.

--- config/routes.rb
+++ config/routes.rb
@@ -56,12 +56,14 @@ Rails.application.routes.draw do
   get 'books/show'
 
   get 'about/index'
+  
+  get 'about' => 'about#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'books#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -71,6 +73,11 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  
+  resources :books
+  resources :authors
+  resources :categories
+  resources :publishers
 
   # Example resource route with options:
   #   resources :products do
